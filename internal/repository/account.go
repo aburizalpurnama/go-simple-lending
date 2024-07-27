db400d42e4cf7a917fc30e3b4f4996974ec51449
rizalpurnama <purnamar9@gmail.com>
2024-07-27 13:38:30 +0700

    add account repository implementation

--- internal/repository/account.go
+++ internal/repository/account.go
@@ -1,8 +1,20 @@
 package repository
 
+var _ Account = new(accountImpl)
+
 type (
 	Account interface {
 		Create()
 		GetById()
 	}
+
+	accountImpl struct{}
 )
+
+func NewAccount() *accountImpl {
+	return &accountImpl{}
+}
+
+func (a *accountImpl) Create() {}
+
+func (a *accountImpl) GetById() {}
